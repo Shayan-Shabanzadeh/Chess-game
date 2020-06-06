d61ee7c2b9ab6bc16cb0f52851fcdb50d8a4436f
ASUS ZENBOOK <shayanshabanzadeh0@gmail.com>
2020-06-06 08:29:39 +0430

    making some comments and working on piece

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -9,31 +9,34 @@ public abstract class Tile {
 
    protected final int tileCoordinate;
    
-   private static final Map<Integer,EmptyTile> EMPTY_TILES=creatAllPossibleEmptyTiles();
-
+   private static final Map<Integer,EmptyTile> EMPTY_TILES_CACHE =creatAllPossibleEmptyTiles();
+   //this HashMap save 64 empty tiles beacuse of creatAllPossibleEmptyTiles method.
+    //creatAllPossibleEmptyTile method is for making 64 Empty tile class and connect those classes with Integer like i.
     private static Map<Integer, EmptyTile> creatAllPossibleEmptyTiles() {
         final Map<Integer,EmptyTile> emptyTileMap=new HashMap<>();
         for(int i=0;i<64;i++) {
             emptyTileMap.put(i, new EmptyTile(i));
         }
-        return emptyTileMap;
+        return EmptyTile;
         //you can make this hashMap Immutable with google library which you need to import.
         //the code for making hashMap Immutable is ImmutableMap.copyOf(#hashMap name).
+        //you can also make this HashMap immutable with jdk libarys with using Collections.unmodifiableMao(#HashMap Name).
     }
 
     private Tile(int tileCoordinate){
         this.tileCoordinate=tileCoordinate;
     }
-
+    //this class is abstract so we cant make any object from it.
+    //creatTile is a method for making Tiles
     public static Tile creatTile(final int tileCoordinate,final Piece piece){
-        return piece!=null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece!=null ? new OccupiedTile(tileCoordinate,piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
     public static final class EmptyTile extends Tile{
 
-        EmptyTile(final int coordinate){
+        private EmptyTile(final int coordinate){
             super(coordinate);
         }
 
@@ -51,7 +54,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile{
        private final Piece piece;
-        public OccupiedTile(int tileCoordinate,Piece piece) {
+        private OccupiedTile(int tileCoordinate,Piece piece) {
             super(tileCoordinate);
             this.piece=piece;
         }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -1,3 +1,11 @@
 package com.chess.engine.pieces;
 public class Piece {
+
+    protected final int piecePossition;
+    protected final Alliance pieceAlliance;
+
+    public Piece(final int piecePossition,final Alliance pieceAlliance) {
+        this.piecePossition = piecePossition;
+        this.pieceAlliance = pieceAlliance;
+    }
 }
